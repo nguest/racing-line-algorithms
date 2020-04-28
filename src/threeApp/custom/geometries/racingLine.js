f83a2ea3dc8a0af9c34f21531d3bc75f8796ede4
Nicholas Guest <nguest@workwithopal.com>
2020-04-27 22:19:17 -0700

    good first stab at Dijkstras algorithm for shortest path
    Signed-off-by: Nicholas Guest <nguest@workwithopal.com>

--- src/threeApp/custom/geometries/racingLine.js
+++ src/threeApp/custom/geometries/racingLine.js
@@ -108,82 +108,90 @@ export const racingLine = (scene) => {
   // const cpPoints = centerLine.getSpacedPoints(steps);
 
   const wpCount = 7;
+  const cpCount = 20;
   const trackHalfWidth = 2;
 
   const b = [
     new THREE.Vector3(0, 0, 10),
     // new THREE.Vector3(0,0,0),
     new THREE.Vector3(10, 0, -10),
-    // new THREE.Vector3(0,0,-20),
-    // new THREE.Vector3(0,0,-30),
-    new THREE.Vector3(0, 0, -40),
-    new THREE.Vector3(10, 0, -80),
+    new THREE.Vector3(-10, 0, -30),
+    new THREE.Vector3(-10, 0, -50),
+
+    new THREE.Vector3(0,0,-70),
+    // new THREE.Vector3(0, 0, -40),
+    // new THREE.Vector3(10, 0, -80),
 
   ];
   const centerLine = new THREE.CatmullRomCurve3(b);
-  const cpPoints = centerLine.getSpacedPoints(wpCount);
-  const { binormals, tangents } = computeFrenetFrames(centerLine, wpCount);
+  const cpPoints = centerLine.getSpacedPoints(cpCount);
+  const { binormals } = computeFrenetFrames(centerLine, cpCount);
   console.log({ binormals, cpPoints });
 
 
-  const matrix = cpPoints.map((cp) => {
-    return new Array(wpCount).fill(null).map((wp, i) => {
+  const matrix = cpPoints.map((cp, i) => {
+    return new Array(wpCount).fill(null).map((wp, j) => {
       const s = (trackHalfWidth * 2) / (wpCount - 1);
-      return cp.clone().sub(binormals[i].clone().multiplyScalar(s * (i - (Math.floor(wpCount / 2)))));
+      return cp.clone().sub(binormals[i].clone().multiplyScalar(s * (j - (Math.floor(wpCount / 2)))));
     });
   });
 
   console.log({ matrix });
+  const path = Dijkstra(matrix);
+  
 
-  const segmentValue = (pMinus1, p, pPlus1) => {
-    const alpha = 0.01;
-    const beta = 0.5;
-    // Beta * Cos( P ) – Alpha * ( A + B );
-    const v1 = p.clone().sub(pMinus1.clone());
-    const v2 = pPlus1.clone().sub(p.clone());
-    const theta = v1.angleTo(v2);
-    // debugger;
-    // console.log(beta * Math.cos(theta));
-    // console.log('1::', pMinus1, p, pPlus1)
-    console.log('2::', (theta / (2 * Math.PI)) * 360);
-    return beta * Math.cos(theta);
-
-
-    // return beta * Math.cos(theta);// - alpha * (v1.length() + v2.length());
-  }
+  // const segmentValue = (pMinus1, p, pPlus1) => {
+  //   const alpha = 0.01;
+  //   const beta = 0.5;
+  //   // Beta * Cos( P ) – Alpha * ( A + B );
+  //   const v1 = p.clone().sub(pMinus1.clone());
+  //   const v2 = pPlus1.clone().sub(p.clone());
+  //   const theta = v1.angleTo(v2);
+  //   // debugger;
+  //   // console.log(beta * Math.cos(theta));
+  //   // console.log('1::', pMinus1, p, pPlus1)
+  //   //console.log('2::', (theta / (2 * Math.PI)) * 360);
+  //   return beta * Math.cos(theta);
 
 
-  const nodes = [];
+  //   // return beta * Math.cos(theta);// - alpha * (v1.length() + v2.length());
+  // }
 
-  for (let i = cpPoints.length - 2; i >= 1; i -= 1) {
-    // each node on i - 1;
-    let brv = 0;
-    let bnn = 0;
-    // nodes[i] = [];
 
-    for (let j = 0; j < wpCount; j++) {
-      // for each node on i
-      for (let k = 0; k < wpCount; k++) {
-        let trv;
-        if (nodes[i + 1] && nodes[i + 1].routeValue) {
-          trv = segmentValue(matrix[i - 1][j], matrix[i][j], matrix[i + 1][j]) + nodes[i + 1].routeValue;
-        } else {
-          trv = segmentValue(matrix[i - 1][j], matrix[i][j], matrix[i + 1][j])
-        }
-        console.log(i, '---', trv);
+  // const nodes = [{ routeValue: 0, nextNode: 3 }, { routeValue: 0, nextNode: 3 }];
 
-        if (trv > brv) {
-          brv = trv;
-          bnn = j;
-          nodes[i] = { routeValue: brv, nextNode: bnn };
-          // console.log(i, '  ', nodes[i]);
+  // for (let i = cpPoints.length - 2; i >= 1; i -= 1) {
+  //   let brv = 0;
+  //   let bnn = 0;
 
-        }
-      }
+  //   nodes[i] = [];
+  //   // for each node on i - 1;
+  //   for (let j = 0; j < wpCount; j++) { //PREV
+  //     // for each node on i + 1
+  //     for (let k = 0; k < wpCount; k++) { //NEXT
+  //       let trv;
+  //       if (nodes[i + 1] && nodes[i + 1].routeValue) {
+  //         trv = segmentValue(matrix[i - 1][j], matrix[i][j], matrix[i + 1][j]) + nodes[i + 1].routeValue;
+  //       } else {
+  //         trv = segmentValue(matrix[i - 1][j], matrix[i][j], matrix[i + 1][j]);
+  //       }
+  //       if (i === cpPoints.length - 2) {
+  //         console.log(i, '---', trv);
 
-    }
-  }
-  console.log({ nodes });
+  //       }
+
+  //       if (trv > brv) {
+  //         brv = trv;
+  //         bnn = j;
+  //         // console.log(i, '  ', nodes[i]);
+
+  //       }
+  //     }
+  //     nodes[i][j] = { routeValue: brv, nextNode: bnn };
+  //   }
+  // }
+  // nodes.reverse()
+  // console.log({ nodes });
   // line materials
   const redMat = new THREE.LineBasicMaterial({ color: 0xff0000 });
   const pinkMat = new THREE.LineBasicMaterial({ color: 0xff5555 });
@@ -196,40 +204,69 @@ export const racingLine = (scene) => {
   const cpLineObj = new THREE.Line(cpGeometry, redMat);
   scene.add(cpLineObj);
 
-  const s = trackHalfWidth * 2;
-  const olPointsL = cpPoints.map((cp, i) => cp.clone().sub(binormals[i].clone().multiplyScalar(s)))
+  const s = trackHalfWidth;
+
+  const olPointsL = cpPoints.map((cp, i) => cp.clone().sub(binormals[i].clone().multiplyScalar(s)));
   const olGeometryL = new THREE.BufferGeometry().setFromPoints(olPointsL);
   const olObjL = new THREE.Line(olGeometryL, redMat);
   scene.add(olObjL);
-  const olPointsR = cpPoints.map((cp, i) => cp.clone().sub(binormals[i].clone().multiplyScalar(-s)))
+  const olPointsR = cpPoints.map((cp, i) => cp.clone().sub(binormals[i].clone().multiplyScalar(-s)));
   const olGeometryR = new THREE.BufferGeometry().setFromPoints(olPointsR);
   const olObjR = new THREE.Line(olGeometryR, redMat);
   scene.add(olObjR);
 
-  const segments = cpPoints.map((cp, i) => {
-    const s = (trackHalfWidth * 2);
-    return [
-      cp.clone().sub(binormals[i].clone().multiplyScalar(s)),
-      cp.clone().sub(binormals[i].clone().multiplyScalar(-s)),
-    ];
-  });
+  const segments = cpPoints.map((cp, i) => ([
+    cp.clone().sub(binormals[i].clone().multiplyScalar(s)),
+    cp.clone().sub(binormals[i].clone().multiplyScalar(-s)),
+  ]));
   segments.forEach((segment) => {
     const geometry = new THREE.BufferGeometry().setFromPoints([...segment]);
     const slObj = new THREE.Line(geometry, pinkMat);
     scene.add(slObj);
   });
 
-  const rlPoints = nodes.map((node, i) => {
-    return [matrix[i][node.nextNode], matrix[i + 1][node.nextNode]];
+  const segments2 = matrix.map((cp, i) => ([
+    ...cp,
+  ]));
+  segments2.forEach((segment) => {
+    const geometry = new THREE.BufferGeometry().setFromPoints([...segment]);
+    const slObj = new THREE.Line(geometry, greenMat);
+    scene.add(slObj);
   });
-  rlPoints.forEach((rlSeg) => {
-    const geometry = new THREE.BufferGeometry().setFromPoints([...rlSeg]);
-    const rlObj = new THREE.Line(geometry, greenMat);
-    scene.add(rlObj);
-  });
 
+  // const rlPoints = nodes.map((node, i) => {
+  //   if (nodes[i+1]) {
+  //     return [matrix[i][node.nextNode], matrix[i + 1][nodes[i+1].nextNode]];
+  //   }
+  //   return [matrix[i][node.nextNode], matrix[i + 1][3]];
+  // });
+  // console.log({ rlPoints });
 
-
+  // rlPoints.forEach((rlSeg) => {
+  //   console.log({ rlSeg });
+    
+  //   const geometry = new THREE.BufferGeometry().setFromPoints([...rlSeg]);
+  //   const rlObj = new THREE.Line(geometry, greenMat);
+  //   scene.add(rlObj);
+  // });
+  console.log({ xxxx: path });
+  
+  path.forEach((rlSeg, idx) => {
+    if (path[idx + 1]) {
+      const point1 = rlSeg.split('-');
+      const point2 = path[idx + 1].split('-');
+      //console.log({ pointString });
+      const p1 = matrix[point1[0]][point1[1]];
+      const p2 = matrix[point2[0]][point2[1]];
+      console.log({ p1 });
+      const geometry = new THREE.BufferGeometry().setFromPoints([p1, p2]);
+      const rlObj = new THREE.Line(geometry, greenMat);
+      scene.add(rlObj);
+    }
+    
+    
+  })
+ 
 };
 
 /*
@@ -249,3 +286,127 @@ For each node Prev on Line 5
     Cur’s RVM [ Prev ].m_routeNextNode = Best Next Node So Far;
 }
 */
+
+class Graph {
+  constructor() {
+    this.nodes = [];
+    this.adjacencyList = {};
+  }
+
+  addNode(node) {
+    this.nodes.push(node);
+    this.adjacencyList[node] = [];
+  }
+
+  addEdge(node1, node2, weight) {
+    this.adjacencyList[node1].push({ node: node2, weight });
+    //this.adjacencyList[node2].push({ node: node1, weight });
+  }
+
+  findPathWithDijkstra(startNode, endNode) {
+    const totalWeights = {};
+    const backtrace = {};
+    const pq = new PriorityQueue();
+
+    totalWeights[startNode] = 0;
+
+    this.nodes.forEach((node) => {
+      if (node !== startNode) {
+        totalWeights[node] = Infinity;
+      }
+    });
+
+    pq.enqueue([startNode, 0]);
+    console.log('after enqueue');
+
+    while (!pq.isEmpty()) {
+      const shortestStep = pq.dequeue();
+
+      const currentNode = shortestStep[0];
+      this.adjacencyList[currentNode].forEach((neighbor) => {
+        const weight = totalWeights[currentNode] + neighbor.weight;
+        console.log({ totalWeights });
+
+        if (weight < totalWeights[neighbor.node]) {
+          totalWeights[neighbor.node] = weight;
+          backtrace[neighbor.node] = currentNode;
+          pq.enqueue([neighbor.node, weight]);
+        }
+      });
+    }
+    console.log('before end');
+
+    const path = [endNode];
+    let lastStep = endNode;
+    while (lastStep !== startNode) {
+      path.unshift(backtrace[lastStep]);
+      lastStep = backtrace[lastStep];
+    }
+    console.log(`Path is ${path} and totalWeight is ${totalWeights[endNode]}`);
+    return path;
+  }
+};
+
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+
+  enqueue(element) {
+    if (this.isEmpty()) {
+      this.collection.push(element);
+    } else {
+      let added = false;
+      for (let i = 1; i <= this.collection.length; i++) {
+        if (element[1] < this.collection[i - 1][1]) {
+          this.collection.splice(i - 1, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        this.collection.push(element);
+      }
+    }
+  }
+
+  dequeue() {
+    const value = this.collection.shift();
+    return value;
+  }
+
+  isEmpty() {
+    return (this.collection.length === 0);
+  }
+}
+
+
+const Dijkstra = (matrix) => {
+  const map = new Graph();
+
+  // add nodes
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      map.addNode(`${i}-${j}`);
+    }
+  }
+  // add edges
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      for (let k = 0; k < matrix[0].length; k++) {
+        let weight;
+        if (matrix[i + 1]) {
+          // we can also weight by curvature
+          weight = matrix[i][j].distanceTo(matrix[i + 1][k]);
+        } else {
+          weight = 0;
+        }
+        map.addEdge(`${i}-${j}`, `${i + 1}-${k}`, weight);
+      }
+    }
+  }
+  console.log({ map });
+  const path = map.findPathWithDijkstra('0-6', '20-0');
+  console.log({ path });
+  return path;
+}
